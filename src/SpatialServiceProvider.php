4e009a22e4984be7694514526b33848fcc5a8fa0
SaadEddinHassania <saadhassaniya@gmail.com>
2019-07-03 01:52:49 +0300

    autoload error when no .env

--- src/SpatialServiceProvider.php
+++ src/SpatialServiceProvider.php
@@ -30,10 +30,11 @@ class SpatialServiceProvider extends DatabaseServiceProvider
         // The connection factory is used to create the actual connection instances on
         // the database. We will inject the factory into the manager so that it may
         // make the connections while they are actually needed and not of before.
-        $this->app->singleton('db.factory', function ($app) {
-            return new ConnectionFactory($app);
-        });
-
+        if (isset($app['db'])) {
+            $this->app->singleton('db.factory', function ($app) {
+                return new ConnectionFactory($app);
+            });
+        }
         // The database manager is used to resolve various connections, since multiple
         // connections might be managed. It also implements the connection resolver
         // interface which may be used by other components requiring connections.
